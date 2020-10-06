eb6b8e5acdfad9b1e6956ab6dd3a2f19953cd849
Erin <erin.gill81@gmail.com>
2020-10-06 10:44:02 -0700

    docs: added my info to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ setup(name='guess_number_oop',
       version='0.1',
       description='Guess a random computer-generated number (OOP practice)',
       packages=['guess_number_oop'],
+      author='Erin Gill',
+      author_email='erin.gill81@gmail.com',
       zip_safe=False)
