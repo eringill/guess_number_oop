fcfc073c9e7385f432e962503131651019a002a8
Erin <erin.gill81@gmail.com>
2020-10-06 21:33:15 -0700

    docs: added inline code comments and docstrings to both modules

--- guess_number_oop/Game.py
+++ guess_number_oop/Game.py
@@ -1,16 +1,22 @@
 import random
 
 class Game():
+    ''' Game class that keeps track of all game attributes'''
     def __init__(self):
-        self.max = 1000
-        self.min = 0
-        self.integer = random.randint(self.min, self.max)
-        self.num_guesses = 0
-        self.total_guesses = 20
-        self.guesses = []
-        self.state = 1
+        self.max = 1000 # max integer for computer to choose
+        self.min = 0 # minimum integer for computer to choose
+        self.integer = random.randint(self.min, self.max) # integer that computer chooses
+        self.num_guesses = 0 # number of guesses already made
+        self.total_guesses = 20 # number of guesses available
+        self.guesses = [] # numbers guessed
+        self.state = 1 # state 1 = in play, state 0 = game over
         
     def make_guess(self):
+        ''' method to make a guess
+        Inputs: integers typed by the user
+        Outputs: whether the guess is higher or lower than the actual integer chosen by computer 
+        (or the correct integer) and
+        the number of guesses left in the game ''' 
         if self.num_guesses < 20:
             
             try:
@@ -19,8 +25,8 @@ class Game():
                 print ("That guess is not valid. Try again.")
  
             else:
-                self.guesses.append(guess)
-                self.num_guesses = len(self.guesses)
+                self.guesses.append(guess) # append guess to list of guesses
+                self.num_guesses = len(self.guesses) # number of guesses = length of guesses list
                 
                 if guess > self.integer:
                     print ("\nThe actual integer is lower than the integer you chose.")
@@ -31,5 +37,6 @@ class Game():
                     print ("\nCongratulations you guessed the number in {} guesses.".format(self.num_guesses))
     
     def __repr__(self):
+        ''' returns the number of guesses left and the numbers guessed so far'''
         return "You have {} guesses left out of {} total guesses.\nSo far you have guessed the numbers {}.\n"\
                 .format((20 - self.num_guesses), self.total_guesses, self.guesses)
--- guess_number_oop/guess_number_oop.py
+++ guess_number_oop/guess_number_oop.py
@@ -1,25 +1,27 @@
 import sys
 from .Game import Game
 
-def play_game():        
-    new_game = Game()
-    print(new_game)
+def play_game():
+'''function to play the number guessing game. All input is given via the keyboard'''        
+    new_game = Game() # initialize a new game
+    print(new_game) # print the status of the game
     
     while new_game.num_guesses < 20 and new_game.state == 1:
+        ''' run this loop until the player makes 20 guesses, or guesses the correct number'''
         new_game.make_guess()
         
         if new_game.state == 1:
-            print(new_game)
+            print(new_game) # print game status if player hasn't won
         
     if new_game.num_guesses == 20:
         new_game.state = 0
         print("Sorry, you ran out of guesses. The computer picked the number {}.".format(new_game.integer))
         
-    print("Would you like to play again? (y/n)")
+    print("Would you like to play again? (y/n)") #prompt the user to play a new game
 
     response = str(input())
 
-    if response == 'y':
+    if response == 'y': # if user says y, play game, else exit program
         play_game()
     else:
         print("Bye!")
@@ -28,10 +30,10 @@ def play_game():
 
 print("GUESS THE NUMBER BETWEEN 0 AND 1,000 IN 20 GUESSES OR LESS!!\n\n")
 
-print("Would you like to play? (y/n)")
+print("Would you like to play? (y/n)") # ask the user if they want to play, else exit program
 response = str(input())
 
-if response == 'y':
+if response == 'y': # if user says y, play game, else exit program
     play_game()
 else:
     print("Bye!")
